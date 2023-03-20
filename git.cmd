203bc5db865af834ffe4b315ce52259c49f90d90
casiaer01 <siasuntml@163.com>
2023-03-20 14:13:23 +0800

    added git.cmd

---
+++ git.cmd
@@ -0,0 +1,8 @@
+get remote add origin xxx.git
+get fetch origin master(remote branch name)
+get checkout -b dev(local branch name) origin/master(remote branch name)
+git pull origin master
+git status
+git add xxx or(.)
+git commit -m "added xxx"
+git push origin dev:master
